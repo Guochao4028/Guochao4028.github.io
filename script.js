6c5e6fddf6fcf24a8d3a47a0760a72b4f45a87f7
guochao <mark_4028@sina.com>
2025-02-28 18:39:02 +0800

    加入其他页面

--- script.js
+++ script.js
@@ -7,6 +7,23 @@ const logoAndTitle = document.getElementById('logoAndTitle');
 if (logoAndTitle) {
     logoAndTitle.addEventListener('click', function () {
         // 这里可以添加你想要执行的代码，例如跳转到首页
-        window.location.href = '/';
+        window.location.href = './index.html';
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const serviceItems = document.querySelectorAll('.service-item');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    serviceItems.forEach(item => {
+        observer.observe(item);
+    });
+});
